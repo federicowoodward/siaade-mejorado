3df2e1e1bc4571238b8eed941c7f76f02860f690
federicowoodward <woodfederico@gmail.com>
2025-07-30 18:32:47 -0300

    paginas de students mejoradas

--- front/src/app/pages/students/appointments_documents_page/appointments-documents-page.ts
+++ front/src/app/pages/students/appointments_documents_page/appointments-documents-page.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-appointments-documents-page',
-  imports: [],
+  imports: [Button],
   templateUrl: './appointments-documents-page.html',
-  styleUrl: './appointments-documents-page.scss'
+  styleUrl: './appointments-documents-page.scss',
 })
 export class AppointmentsDocumentsPage {
-
+  
+  downloadCertificate() {
+    //integrar servicio de generacion y datos de usuario
+    window.open('assets/doc.pdf', '_blank');
+  }
 }
