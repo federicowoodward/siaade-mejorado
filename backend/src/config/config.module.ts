0d3336a069cf3d299b525b507c47173341dd016b
federicowoodward <woodfederico@gmail.com>
2025-09-03 19:43:22 -0300

    SCRUM-418: migracion de seed creada

--- backend/src/config/config.module.ts
+++ backend/src/config/config.module.ts
@@ -1,13 +1,10 @@
-
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule as NestConfigModule, ConfigService } from '@nestjs/config';
-import { User } from '../entities/users.entity';
-import { Subject } from '../entities/subjects.entity';
-import { FinalExam } from '../entities/final-exam.entity';
-import { Role } from '../entities/roles.entity';
-import { Student } from '../entities/students.entity';
-import { Exam } from '../entities/exams.entity';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import {
+  ConfigModule as NestConfigModule,
+  ConfigService,
+} from "@nestjs/config";
+import * as path from "path";
 
 @Module({
   imports: [
@@ -16,13 +13,13 @@ import { Exam } from '../entities/exams.entity';
       imports: [NestConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: parseInt(config.get('DB_PORT', '5432'), 10),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_DATABASE'),
-        entities: [User, Subject, FinalExam, Role, Student, Exam],
+        type: "postgres",
+        host: config.get("DB_HOST"),
+        port: parseInt(config.get("DB_PORT", "5432"), 10),
+        username: config.get("DB_USERNAME"),
+        password: config.get("DB_PASSWORD"),
+        database: config.get("DB_DATABASE"),
+        entities: [path.resolve(__dirname, "..", "entities", "*.{ts,js}")],
         synchronize: false,
         logging: true,
       }),
