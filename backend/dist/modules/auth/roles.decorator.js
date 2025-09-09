7acc74fb9653cc041454a138e2f85bddd694a132
JooellH <maurivictorholz@gmail.com>
2025-09-09 16:16:34 -0300

    chore(deploy): ajustes backend para Railway (config, CORS, scripts, fix import reset-password)

--- backend/dist/modules/auth/roles.decorator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = exports.ROLES_KEY = void 0;
-const common_1 = require("@nestjs/common");
-exports.ROLES_KEY = 'roles'; // Clave para acceder a los roles en el metadata
-const Roles = (...roles) => (0, common_1.SetMetadata)(exports.ROLES_KEY, roles); // El decorador para asignar roles
-exports.Roles = Roles;
-//Este decorador Roles va a recibir una lista de roles y los va a agregar como metadata en la ruta que lo utilices. Esto nos permitirá verificar qué roles tienen acceso a una ruta en particular.
-//# sourceMappingURL=roles.decorator.js.map
