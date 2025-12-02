fb000dd559edee150305c576e47adc8eae1623eb
federicowoodward <woodfederico@gmail.com>
2025-12-02 20:19:55 -0300

    feat: faq and tyc implemented

---
+++ front/src/app/pages/termns-and-conditions/termns-and-conditions.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppBreadcrumbComponent } from "@/shared/components/breadcrumb/app-breadcrumb.component";
+
+@Component({
+  selector: 'app-termns-and-conditions',
+  standalone: true,
+  imports: [CommonModule, AppBreadcrumbComponent],
+  templateUrl: './termns-and-conditions.html',
+  styleUrl: './termns-and-conditions.scss',
+})
+export class TermnsAndConditions {}
